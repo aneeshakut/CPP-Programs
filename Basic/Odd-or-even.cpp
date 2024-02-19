309611587837c2c71407a76718af99f5dee5f223
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-02-19 10:37:40 +0530

    Create Odd-or-even.cpp

---
+++ Basic/Odd-or-even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    std::cin>>n;
+
+    if(n%2==0){
+        std::cout<<"Even";
+    }else{
+        std::cout<<"Odd";
+    }
+    return 0;
+}
