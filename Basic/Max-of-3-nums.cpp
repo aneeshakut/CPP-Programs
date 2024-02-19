ef9535e7bfa4295d4af0611f069a6ce3d7e1a3c7
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-02-19 10:34:09 +0530

    Create Max-of-3-nums.cpp

---
+++ Basic/Max-of-3-nums.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    std::cin>>a>>b>>c;
+    
+    if(a>b){
+        if(a>c){
+            std::cout<<a;
+        }else{
+            std::cout<<c;
+        }
+    }else{
+        if(b>c){
+            std::cout<<b;
+        }else{
+            std::cout<<c;
+        }
+    }
+    return 0;
+}
