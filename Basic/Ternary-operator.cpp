9f40098c951ce4683e8b19d98ae23069bc9ea6c6
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-02-19 11:37:01 +0530

    Create Ternary-operator.cpp

---
+++ Basic/Ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    bool adult;
+    cin>>age;
+    (age>=18) ? adult=true : adult=false ;
+    cout<<adult;
+    return 0;
+}
