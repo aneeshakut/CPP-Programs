38661869244569b9bb5a7ed8311429b4c3368639
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-03-08 19:32:41 +0530

    Create Access-Stack.cpp

---
+++ STL/Stack/Access-Stack.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout<<s.top(); //Last In - First Out
+    return 0;
+}
